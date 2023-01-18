e8d76ac7edaee441149bb15adb30f2c862c48583
NSIST96 <amelin.good@mail.ru>
2023-01-18 19:14:18 +0300

    first fixed version

---
+++ src/Components/Content/Content.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import classes from './Content.module.css'
+
+const Content = (props) =>{
+    return(
+        <div className={classes.Content}>
+            Content
+        </div>
+    )
+}
+
+export default Content
---
+++ src/Components/Header/Header.jsx
@@ -0,0 +1,19 @@
+import React from 'react'
+import logo from './../../Images/logo.jpg'
+import classes from './Header.module.css'
+
+const Header = (props) =>{
+    return(
+        <div className={classes.Header}>
+            <div className={classes.header_input}>
+                <img src={logo} alt='logo'></img>
+                <div className={classes.header_phone}><h4>Тел.: +7(910)592-11-47</h4></div>
+                <div className={classes.header_main}><h3><a href='#'>Главная</a></h3></div>
+                <div className={classes.header_news}><h3><a href='#'>Новости</a></h3></div>
+                <div className={classes.header_contacts}><h3><a href='#'>Контакты</a></h3></div>
+            </div>
+        </div>
+    )
+}
+
+export default Header
